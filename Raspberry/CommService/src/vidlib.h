e74db5253cde7cce2eac81c1e05cccac14979ee9
Leland Miller <lelandmiller@gmail.com>
2014-09-16 21:18:28 -0700

    Updates to CommService

---
+++ Raspberry/CommService/src/vidlib.h
@@ -0,0 +1,2 @@
+
+int send_video_directory(char* cam_name, char* dir_name, char* remote_host, char* remote_port);
---
+++ Raspberry/CommService/src/vidsender2.c
@@ -0,0 +1,4 @@
+#include "vidlib.h"
+int main(int argc, char** argv) {
+   send_video_directory("SlugCam1", "./vids", "skynet.soe.ucsc.edu", "7893"); 
+}
