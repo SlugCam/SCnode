9bea84879676a4aa0de33557128b13f415754a50
Kevin Abas <kabas12@gmail.com>
2015-02-19 12:51:24 -0800

    cleanup

--- MSP430ProjectFiles/RelayTest/RelayTest.ino
+++ MSP430ProjectFiles/RelayTest/RelayTest.ino
@@ -9,36 +9,53 @@
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output. and the relay set to off
-   digitalWrite(10, HIGH);
-   digitalWrite(9, LOW);
+   //digitalWrite(10, LOW);
+   //digitalWrite(9, HIGH);
    pinMode(10,OUTPUT);
    pinMode(9,OUTPUT);
+  
    
    pinMode(PUSH2, INPUT_PULLUP);
    attachInterrupt(PUSH2, off, FALLING); // Interrupt is fired whenever button is pressed
    
-   pinMode(P2_3, INPUT);
-   attachInterrupt(P2_3, motion, RISING); // Interrupt is fired when motion is detected
+   //pinMode(11, INPUT);
+   //attachInterrupt(11, motion, RISING); // Interrupt is fired when motion is detected
+   
+   
 
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-              
+    if(digitalRead(9) == HIGH){
+         digitalWrite(10, HIGH);
+         digitalWrite(9, LOW);
+    }else{
+        digitalWrite(10, LOW);
+         digitalWrite(9, HIGH);
+    } 
+   delay(5000); 
    
 }
 
 //push button's interupt routine
 void off(){
-   digitalWrite(10, HIGH);
-   digitalWrite(9, LOW);
+    if(digitalRead(9) == HIGH){
+         digitalWrite(10, HIGH);
+         digitalWrite(9, LOW);
+    }else{
+        digitalWrite(10, LOW);
+         digitalWrite(9, HIGH);
+    }  
+   
+  
 }
 
 //PIR's interupt routine
 void motion(){
-  if(digitalRead(10) == HIGH){
-         digitalWrite(10, LOW);
-         digitalWrite(9, HIGH);
+  if(digitalRead(9) == HIGH){
+         digitalWrite(10, HIGH);
+         digitalWrite(9, LOW);
   }  
   
 }
